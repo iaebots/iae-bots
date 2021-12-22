f590a516aa7ad2e6243f172ec875456a8072a156
Utzig26 <gabriel.utzig26@gmail.com>
2021-12-22 15:10:37 -0300

    post in twitter until post 700

--- peixes_fodas/main.py
+++ peixes_fodas/main.py
@@ -3,8 +3,16 @@ import os
 import searchImages
 import requests
 import fishManager
+import tweepy
 
 #CONSTANTES
+
+TWITTER_ACCESS_KEY = str(os.environ.get('TWITTER_PEIXESFODAS_ACCESSKEY'))
+TWITTER_ACCESS_SECRET = str(os.environ.get('TWITTER_PEIXESFODAS_ACCESSSECRET'))
+
+TWITTER_CONSUMER_KEY = str(os.environ.get('TWITTER_PEIXESFODAS_CONSUMERKEY'))
+TWITTER_CONSUMER_SECRET = str(os.environ.get('TWITTER_PEIXESFODAS_CONSUMERSECRET'))
+
 IAE_API_KEY = str(os.environ.get('IAE_PEIXESFODAS_APIKEY'))
 IAE_API_SECRET = str(os.environ.get('IAE_PEIXESFODAS_APISECRET'))
 URL = 'https://api.iaebots.com/api/v1/posts'
@@ -18,8 +26,31 @@ def iaePost(image, data, API_KEY, API_SECRET):
         return response
 
 #TwitterPost
-#def twitterPost():
-    #...
+def twitterPost(image, status, ACCESS_KEY, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET):
+    print(ACCESS_KEY)
+    print(ACCESS_SECRET)
+    print(CONSUMER_KEY)
+    print(CONSUMER_SECRET)
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+    api = tweepy.API(auth)
+    filename = 'temp.jpg'
+    request = requests.get(image, stream=True)
+    if request.status_code == 200:
+        with open(filename, 'wb') as img:
+            for chunk in request:
+                img.write(chunk)
+        try:
+            api.update_status_with_media(status=status, filename=filename)
+            print('tentativa')
+            os.remove(filename)
+            return True
+        except Exception as e:
+            print(e)
+            os.remove(filename)   
+            return False
+    else:
+        return False
 
 #Finding Nemo
 def main():
@@ -27,19 +58,24 @@ def main():
     conts = fm.numFishSelector()
     fish = fm.fishSelector(conts[0])
     print(fish)
-    data = {'body': "Peixe foda #" + str(conts[1]) + " - " + fish}
+    status = "Peixe foda #" + str(conts[1]) + " - " + fish
+    data = {'body': status}
     images = searchImages.get_google_images_data(fish)
     if len(images) == 0:
         fm.fishUpdate(conts, False)
         main()
     for image in images:
         try:
-            response = iaePost(image, data, IAE_API_KEY, IAE_API_SECRET)
+            #responseI = iaePost(image, data, IAE_API_KEY, IAE_API_SECRET)
+            responseT = twitterPost(image, status, TWITTER_ACCESS_KEY, TWITTER_ACCESS_SECRET, TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
+            print(responseT)
         except:
             continue
-        if response.ok:
-            print(response)
-            fm.fishUpdate(conts, response.ok)
+        #if responseI.ok and responseT.id:
+        if responseT == True:
+            #print(responseI)
+            print(responseT)
+            fm.fishUpdate(conts, True)
             break      
 
 if __name__ == "__main__":
